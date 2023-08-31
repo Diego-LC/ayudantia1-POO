f4d25a98b27c3f282dbd2c6dd2a9c39b7c136c6f
DIΣGӨ L <di3golabrin@gmail.com>
2023-08-31 15:06:39 -0400

    Primer commit de la actividad

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+    //Método 1:
+    public static boolean metodo1(String palabraIngresada) {
+        palabraIngresada = palabraIngresada.replace(" ", "a");
+        StringBuilder stringBuilder = new StringBuilder(palabraIngresada);
+        String palabraInvertida = stringBuilder.reverse().toString();
+        return palabraIngresada.equalsIgnoreCase(palabraInvertida);
+    }
+    //Método 2
+    public static double metodo2(double [] notas) {
+        double [] ponderacionNotas = {0.25, 0.25, 0.25, 0.15, 0.10};
+        double promedio = 0;
+        for (int i = 0; i < notas.length; i++) {
+            promedio = (notas[i] * ponderacionNotas[i]);
+        }
+        return promedio;
+    }
+    //Método 3:
+    private static String metodo3(int [] puntajes) {
+        int jugador1 = 20 - puntajes[0];
+        int jugador2 = 20 - puntajes[1];
+        if((jugador1 < 0 && jugador2 < 0) || jugador1 == jugador2) {
+            return "Empate.";
+        }
+        else if((jugador1 < 0 || jugador2 < jugador1) && jugador2 >= 0) {
+            return "Jugador 2 ganó.";
+        } else {
+            return "Jugador 1 ganó.";
+        }
+    }
+    //Método 4:
+    public static int[][] metodo4(int[][] a) {
+        int[][] t = new int[a[0].length][a.length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                t[j][i] = a[i][j];
+            }
+        }
+        return t;
+    }
+    //Método 5:
+    public static boolean metodo5(String palabraIngresada) {
+        for (int i = 0; i <= palabraIngresada.length(); i++) {
+            if (Character.isDigit(palabraIngresada.charAt(i)) || palabraIngresada.isBlank()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
